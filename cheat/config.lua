d67cb06fd07a1c7237c1c05b0ea2017d4dc5ea2d
BuildTools <unconfigured@null.spigotmc.org>
2024-09-19 23:21:38 +0200

    Removed config from main file

---
+++ cheat/config.lua
@@ -0,0 +1,36 @@
+-- Change anything if you don't like the default settings. You could also change the statments to be automatically true
+
+
+-- ESP, Aimbot, and Misc Settings
+    espSettings = espSettings or {
+        -- ESP Settings
+        enabled = true, -- Just keep this value true, unless you want to manually activate the ESP/Aimbot.
+        showName = false,
+        showHealth = false,
+        showWeapon = false,
+        showSnaplines = false,
+        showDistance = false,
+        show2DBox = false, -- Option to toggle 2D Box separately
+        showChams = false, -- Chams (see-through walls)
+        showRole = false, -- Show player role (if available)
+        skeletonEnabled = false,
+
+        -- Other Settings
+        distanceLimit = 3000, -- Default distance set to 3000 units
+        distanceUnit = "Units", -- Default distance unit in meters
+
+        -- Aimbot and FOV Settings
+        aimbotEnabled = false, -- Toggle aimbot
+        aimSmoothness = 0.1, -- Smoothing factor for aimbot aim movement
+        aimFOV = 200, -- Field of view radius for the aimbot and FOV circle
+        aimBone = "ValveBiped.Bip01_Head1", -- Bone to aim at (head by default)
+        maxDistance = 2500, -- Default Maximum distance for the aimbot to work
+        showFOVCircle = false, -- FOV Circle Toggle
+        checkVisibility = true,
+
+        -- Misc Settings
+        adjustFOV = 100,
+
+
+
+    }
--- gmod_2_better-ish.lua
+++ gmod_2_better-ish.lua
@@ -5,40 +5,7 @@
 -- Original Code:
 if CLIENT then -- Ensure the script only runs client-side
 
-    -- ESP, Aimbot, and Misc Settings
-    espSettings = espSettings or {
-        -- ESP Settings
-        enabled = false,
-        showName = false,
-        showHealth = false,
-        showWeapon = false,
-        showSnaplines = false,
-        showDistance = false,
-        show2DBox = false, -- Option to toggle 2D Box separately
-        showChams = false, -- Chams (see-through walls)
-        showRole = false, -- Show player role (if available)
-        skeletonEnabled = false,
-
-        -- Other Settings
-        distanceLimit = 3000, -- Default distance set to 3000 units
-        distanceUnit = "Units", -- Default distance unit in meters
-
-        -- Aimbot and FOV Settings
-        aimbotEnabled = false, -- Toggle aimbot
-        aimSmoothness = 0.1, -- Smoothing factor for aimbot aim movement
-        aimFOV = 200, -- Field of view radius for the aimbot and FOV circle
-        aimBone = "ValveBiped.Bip01_Head1", -- Bone to aim at (head by default)
-        maxDistance = 2500, -- Default Maximum distance for the aimbot to work
-        showFOVCircle = false, -- FOV Circle Toggle
-        checkVisibility = true,
-
-        -- Misc Settings
-        adjustFOV = 100,
-
-
-
-    }
-
+    
     -- Menu state
     espMenuOpen = false
     lastToggleTime = 0 -- Variable to track the last time F2 was pressed
